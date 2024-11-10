39656d9c2ca6d0fdd593a9b2f32efaf7d3d551ab
chandanK <iamchandankumar02@gmail.com>
2024-11-10 20:01:27 +0530

    update

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ const Header=()=>{
         <div className="bg-yellow-400 bg-gradient-to-l from-black">
             <div>
            
-                <img alt="img" className="w-[100px] h-[80px] rounded-lg opacity-30 " src=" https://images-platform.99static.com//3cAFNMIJLmYgtYdMGPvlGU6IbPg=/516x287:1516x1290/fit-in/500x500/projects-files/71/7122/712217/a0fe9136-b485-4f2c-9cfb-7dd4a98c8753.png"></img>
+                <img alt="img" className="w-[100px] h-[80px] rounded-sm opacity-30 " src=" https://images-platform.99static.com//3cAFNMIJLmYgtYdMGPvlGU6IbPg=/516x287:1516x1290/fit-in/500x500/projects-files/71/7122/712217/a0fe9136-b485-4f2c-9cfb-7dd4a98c8753.png"></img>
                 {/* <img alt="img" src=" ../public/movielogo.png"></img> */}
             </div>
         </div>
