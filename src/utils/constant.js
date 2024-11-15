bed532a4bfae535c277da53b449a90e375954be9
chandanK <iamchandankumar02@gmail.com>
2024-11-15 19:22:31 +0530

    changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,12 +1,11 @@
 
-
 export const LOGO=" https://images-platform.99static.com//3cAFNMIJLmYgtYdMGPvlGU6IbPg=/516x287:1516x1290/fit-in/500x500/projects-files/71/7122/712217/a0fe9136-b485-4f2c-9cfb-7dd4a98c8753.png"
 
 export const API_OPTIONS={
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYmUxNzFkZWZkNjY5NDc2YzYyYmM3NzYxZTlkMDVjYSIsIm5iZiI6MTczMTY2OTYzOC4yNTk0MzIsInN1YiI6IjY3MzMzYTYxZTJjYjYyYjViYzE2NTNjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a_IMSwnDebTBVbpXbUgXw7CJxDHN_cykAoW5sPSUvxg'
+      Authorization: 'Bearer '
     }
   };
 
